491b5e2a43d47a740c3d840e2dedfd1ec5f35884
fito_segrera <fitosegrera@gmail.com>
2022-08-19 00:49:36 -0500

    Removed sveltekit:reload from primary button light and dark versions

--- src/components/button/dark/lg/primary.svelte
+++ src/components/button/dark/lg/primary.svelte
@@ -5,7 +5,7 @@
 	export let target;
 </script>
 
-<a sveltekit:prefetch sveltekit:reload href={url} alt={label} {target}>
+<a sveltekit:prefetch href={url} alt={label} {target}>
 	<div
 		class="flex h-96 w-auto items-center justify-center border-8 border-primary-main py-32 text-center font-bold text-primary-main hover:cursor-pointer hover:bg-primary-main hover:text-primary-dark sm:px-24 sm:text-p2 md:px-32 md:text-p1">
 		<p>{label}</p>
--- src/components/button/light/lg/primary.svelte
+++ src/components/button/light/lg/primary.svelte
@@ -5,7 +5,7 @@
 	export let target;
 </script>
 
-<a sveltekit:prefetch sveltekit:reload href={url} alt={label} {target}>
+<a sveltekit:prefetch href={url} alt={label} {target}>
 	<div
 		class="flex h-96 w-auto items-center justify-center border-8 border-primary-dark px-32 py-32 text-lg font-bold text-primary-dark hover:cursor-pointer hover:bg-primary-dark hover:text-primary-main">
 		<p>{label}</p>
