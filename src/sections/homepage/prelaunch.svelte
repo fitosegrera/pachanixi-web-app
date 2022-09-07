422d1c8cf1da85d5dbf8406224ef7064e8b92531
fito_segrera <fitosegrera@gmail.com>
2022-09-07 13:22:14 -0500

    Parallax effect and text animation on Prelaunch Page

--- src/sections/homepage/prelaunch.svelte
+++ src/sections/homepage/prelaunch.svelte
@@ -1,4 +1,8 @@
 <script>
+	//LIBS
+	import { onMount } from 'svelte';
+	import Saos from 'saos';
+
 	//CONTAINERS
 	import Main from '../../containers/main.svelte';
 
@@ -9,9 +13,21 @@
 	//PORPS
 	export let data;
 	//console.log(data);
+
+	onMount(async () => {
+		document.addEventListener('mousemove', parallax);
+	});
+
+	const parallax = (event) => {
+		let vid = document.getElementById('nft');
+		const vidPos = 3;
+		const vidX = (window.innerWidth - event.pageX * vidPos) / 90;
+		const vidY = (window.innerHeight - event.pageY * vidPos) / 90;
+		vid.style.transform = `translateX(${vidX}px) translateY(${vidY}px)`;
+	};
 </script>
 
-<div id="wrapper" class="h-auto w-auto sm:mb-72 md:mb-96 lg:mb-124">
+<div id="wrapper" class="wrapper h-auto w-auto sm:mb-72 md:mb-96 lg:mb-124">
 	<!-- <div id="img-wrapper" class="h-full w-full" /> -->
 	<div id="sketch-wrapper" class="h-full w-full">
 		<Starfield startColor={'#000000'} endColor={'#02232B'} speed={'3'} />
@@ -33,12 +49,14 @@
 			</div> -->
 
 			<div class="flex h-auto w-auto justify-center sm:mt-32 md:mt-8">
-				<video width="99%" autoplay loop muted>
+				<video id="nft" width="99%" autoplay loop muted>
 					<source src="/assets/vids/alpha/hero-alpha.webm" type="video/webm" />
 				</video>
 			</div>
 		</div>
-		<div class="">
+		<Saos
+			once={true}
+			animation={'slide-in-elliptic-top-fwd 1s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 			<p
 				class="text-center font-bold text-primary-light sm:mt-32 sm:mb-48 sm:px-16 sm:text-p3 md:px-72 md:text-p2 lg:mt-0 lg:mb-72 lg:px-124 lg:text-p1 xl:px-224">
 				{data.quote_paragraph[0].text}
@@ -47,7 +65,7 @@
 				class="text-center font-semibold text-primary-main sm:px-16 sm:text-p4 md:text-p3 lg:px-124 lg:text-p2 xl:px-224">
 				{data.author_paragraph[0].text}
 			</p>
-		</div>
+		</Saos>
 	</Main>
 </div>
 
@@ -86,5 +104,18 @@
 
 	video::-internal-media-controls-overlay-cast-button {
 		display: none;
+	}
+
+	@keyframes -global-slide-in-elliptic-top-fwd {
+		0% {
+			transform: translateY(-600px) rotateX(-30deg) scale(0);
+			transform-origin: 50% 100%;
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0) rotateX(0) scale(1);
+			transform-origin: 50% 1200px;
+			opacity: 1;
+		}
 	}
 </style>
